c3341f372b7a17ce81ecb9f4f0788ed64e61121e
luis <luishsilva09@gmail.com>
2022-07-06 20:54:22 -0300

    busca produto pelo id no banco de dados

---
+++ src/controllers/productController.js
@@ -0,0 +1,24 @@
+import { ObjectId } from "mongodb";
+import db from "../database/mongo.js";
+
+export async function product(req, res) {
+  const productId = req.params.id;
+  try {
+    const itemData = await db
+      .collection("products")
+      .findOne({ _id: new ObjectId(productId) });
+    res.status(200).send(itemData);
+  } catch {
+    res.sendStatus(500);
+  }
+}
+export default async function addProduct(req, res) {
+  try {
+    const productData = req.body;
+    const p = await db.collection("products").insertOne(req.body);
+    console.log(p);
+    res.sendStatus(200);
+  } catch {
+    res.sendStatus(500);
+  }
+}
