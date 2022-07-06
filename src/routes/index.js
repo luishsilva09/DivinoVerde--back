2cf5fb32c1e3efb04bb3433e357b86263ac44075
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-07-06 19:11:35 -0300

    implement login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 import express from "express";
 
+import { signIn } from '../controllers/authController.';
+
 const router = express.Router();
+
+router.post('/sign-in', signIn);
 
 export default router;
