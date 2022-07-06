9ae5db36df790e38dc56f64376c7c48d33bb66b1
luis <luishsilva09@gmail.com>
2022-07-06 15:42:19 -0300

    conectando banco de dados

---
+++ src/database/mongo.js
@@ -0,0 +1,9 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGO_URI);
+await client.connect();
+const db = client.db("db.divino");
+
+export default db;
