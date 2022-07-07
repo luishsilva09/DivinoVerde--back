0f8a0e183a97cedb5294c5554493635f200bf768
luis <luishsilva09@gmail.com>
2022-07-07 20:42:15 -0300

    adicionando midlleware para validar usuario

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -2,51 +2,42 @@ import { ObjectId } from "mongodb";
 import db from "../database/mongo.js";
 
 export async function AddCart(req, res) {
-  const itemId = req.params.itemId;
-  const { authorization } = req.headers;
+  try {
+    const itemId = req.params.itemId;
+    const session = req.locals.session;
 
-  // const token = authorization?.replace("Bearer ", "");
-  // const session = await db.collection("session").findOne({ token });
-  // if (!token || !session) {
-  //   return res.sendStatus(401);
-  // }
-
-  const itemData = await db
-    .collection("products")
-    .findOne({ _id: new ObjectId(itemId) });
-  await db
-    .collection("users")
-    .updateOne(
-      { _id: new ObjectId("62c742dbb77da7d13facefaf") },
-      { $push: { cart: { ...itemData } } }
-    );
-  //testes
-  const fim = await db
-    .collection("users")
-    .findOne({ _id: new ObjectId("62c742dbb77da7d13facefaf") });
-  res.send(fim);
+    const itemData = await db
+      .collection("products")
+      .findOne({ _id: new ObjectId(itemId) });
+    await db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(session.userId) },
+        { $push: { cart: { ...itemData } } }
+      );
+  } catch {
+    res.sendStatus(500);
+  }
 }
 
 export async function GetCart(req, res) {
-  const userId = req.params.userId;
-  const { authorization } = req.headers;
-  let total = 0;
+  try {
+    const userId = req.params.userId;
+    const session = req.locals.session;
+    let total = 0;
 
-  // const token = authorization?.replace("Bearer ", "");
-  // const session = await db.collection("session").findOne({ token });
-  // if (!token || !session) {
-  //   return res.sendStatus(401);
-  // }
+    const userData = await db
+      .collection("users")
+      .findOne({ _id: new ObjectId(session.userId) });
 
-  const userData = await db
-    .collection("users")
-    .findOne({ _id: new ObjectId("62c742dbb77da7d13facefaf") });
+    userData.cart.map((e) => (total += e.price));
+    const datauser = {
+      userData: userData.cart,
+      total,
+    };
 
-  userData.cart.map((e) => (total += e.price));
-  const datauser = {
-    userData: userData.cart,
-    total,
-  };
-
-  res.send(datauser);
+    res.send(datauser);
+  } catch {
+    res.sendStatus(500);
+  }
 }
